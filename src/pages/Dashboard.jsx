5ad78f5a1a99dbc8f4c10c6c059f491efcdc7219
HNHAI Dev <hongochai0401@gmail.com>
2021-08-31 14:58:30 +0700

    add code dashboard chart, card

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,93 @@
 import React from "react";
+import Chart from "react-apexcharts";
+import { Link } from "react-router-dom";
+
+import statusCards from "../assets/JsonData/status-card-data.json";
+import StatusCard from "../components/status-card/StatusCard";
+
+const chartOptions = {
+  series: [
+    {
+      name: "Online Customers",
+      data: [40, 70, 20, 90, 36, 80, 30, 91, 60],
+    },
+    {
+      name: "Store Customers",
+      data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10],
+    },
+  ],
+  options: {
+    color: ["#6ab04c", "#2980b9"],
+    chart: {
+      background: "transparent",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+      ],
+    },
+    legend: {
+      position: "top",
+    },
+    grid: {
+      show: false,
+    },
+  },
+};
 
 const Dashboard = () => {
-  return <div>DAshboard</div>;
+  return (
+    <div>
+      <h2 className="page-header">Dashboard</h2>
+      <div className="row">
+        <div className="col-6">
+          <div className="row">
+            {statusCards.map((item, index) => (
+              <div className="col-6" key={index}>
+                <StatusCard {...item} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="card full-height">
+            <Chart
+              options={chartOptions.options}
+              series={chartOptions.series}
+              type="line"
+              height="100%"
+            />
+          </div>
+        </div>
+
+        <div className="col-4">
+          <div className="card">
+            <div className="card__header">
+              <h3>top customers</h3>
+            </div>
+            <div className="card__body">{/* Body */}</div>
+            <div className="card__footer">
+              <Link to="/">view all</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
