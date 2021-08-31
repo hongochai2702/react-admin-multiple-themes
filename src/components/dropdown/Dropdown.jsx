36fa279e5d053ccd489c79e17de75515ef6dd568
HNHAI Dev <hongochai0401@gmail.com>
2021-08-31 13:39:53 +0700

    add topbar layout

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./dropdown.css";
 
-const Dropdown = () => {
-  return <div>Down</div>;
+const useToggleDropdown = ({ contentRef, toggleRef }) => {
+  useEffect(() => {
+    document.addEventListener("mousedown", (e) => {
+      if (toggleRef.current && toggleRef.current.contains(e.target)) {
+        contentRef.current.classList.toggle("active");
+      } else {
+        if (toggleRef.current && !toggleRef.current.contains(e.target)) {
+          contentRef.current.classList.remove("active");
+        }
+      }
+    });
+  }, [contentRef, toggleRef]);
+};
+
+const Dropdown = (props) => {
+  const dropdownToggleElm = useRef();
+  const dropdownContentElm = useRef();
+
+  useToggleDropdown({
+    contentRef: dropdownContentElm,
+    toggleRef: dropdownToggleElm,
+  });
+
+  return (
+    <div className="dropdown">
+      <button ref={dropdownToggleElm} className="dropdown__toggle">
+        {props.icon && <i className={props.icon} />}
+        {props.badge && (
+          <span className="dropdown__toggle-badge">{props.badge}</span>
+        )}
+        {props.customToggle}
+      </button>
+      <div ref={dropdownContentElm} className="dropdown__content">
+        {props.renderItems ? props.renderItems : ""}
+        {props.renderFooter && (
+          <div className="dropdown__footer">{props.renderFooter}</div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;
--- src/components/topnav/TopNav.jsx
+++ src/components/topnav/TopNav.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { Link } from "react-router-dom";
 import Dropdown from "../dropdown/Dropdown";
 
 import "./topnav.css";
+import notifications from "../../assets/JsonData/notification.json";
+import userImage from "../../assets/images/tuat.png";
+import userMenus from "../../assets/JsonData/user_menus.json";
+
+const userCurrent = {
+  display_name: "Hai Ho",
+  image: userImage,
+};
+
+const UserToggle = ({ user }) => {
+  return (
+    <div className="topnav__right-user">
+      <div className="topnav__right-user__image">
+        <img src={user.image} alt={user.display_name} />
+      </div>
+      <div className="topnav__right-user__name">{user.display_name}</div>
+    </div>
+  );
+};
+
+const UserMenuItem = ({ item, index }) => {
+  return (
+    <Link to="/" key={index}>
+      <div className="notification-item">
+        <i className={item.icon}></i>
+        <span>{item.content}</span>
+      </div>
+    </Link>
+  );
+};
+const NotificationItem = ({ item, index }) => {
+  return (
+    <div className="notification-item" key={index}>
+      <i className={item.icon}></i>
+      <span>{item.content}</span>
+    </div>
+  );
+};
 
 const TopNavSearch = () => {
   return (
@@ -13,18 +52,34 @@ const TopNavSearch = () => {
 };
 
 const TopNavRight = () => {
+  const notificationItemRender = useMemo(() => {
+    return notifications.map((item, index) => (
+      <NotificationItem key={index} item={item} index={index} />
+    ));
+  }, []);
+  const userMenuItemRender = useMemo(() => {
+    return userMenus.map((item, index) => (
+      <UserMenuItem key={index} item={item} index={index} />
+    ));
+  }, []);
+
   return (
     <div className="topnav-right">
       <div className="topnav__right-item">
-        {/* dropdown item */}
-        <Dropdown />
+        <Dropdown
+          customToggle={<UserToggle user={userCurrent} />}
+          renderItems={userMenuItemRender}
+        />
       </div>
       <div className="topnav__right-item">
-        {/* dropdown item */}
-        <Dropdown />
+        <Dropdown
+          icon="bx bx-bell"
+          badge="12"
+          renderItems={notificationItemRender}
+          renderFooter={<Link to="/">View All</Link>}
+        />
       </div>
       <div className="topnav__right-item">
-        {/* theme setting */}
         <Dropdown />
       </div>
     </div>
